077321c05f9738e9d2fefcd409fe6067c8d16577
gerblesh <101901964+gerblesh@users.noreply.github.com>
2023-05-20 21:46:25 -0700

    fix autostart.sh and hopefully fixed yafti

--- usr/share/ublue-os/firstboot/launcher/autostart.sh
+++ usr/share/ublue-os/firstboot/launcher/autostart.sh
@@ -1,8 +1,11 @@
 #!/usr/bin/env bash
 
-cp -r /usr/etc/homedir/* $"HOME"/
+cp -r /usr/etc/homedir/* "$HOME"/
+cp -r /usr/etc/homedir/.config/* "$HOME"/.config/
+cp -r /usr/etc/homedir/.local/share/* "$HOME"/.local/share/
 
 Hyprland
 
+sleep 10
 # Simply launches the "yafti" GUI with the uBlue image's configuration.
 /usr/bin/yafti /usr/share/ublue-os/firstboot/yafti.yml
