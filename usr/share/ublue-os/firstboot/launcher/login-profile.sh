6918814178d6f305f5d34d51b8fca8e23c04e32d
gerblesh <101901964+gerblesh@users.noreply.github.com>
2023-05-21 00:49:24 -0700

    FINALLY fixed issues with the firstboot process, updated yafti to include more programs I use, probably will add scripts to change flatpak gtk themes + flatpak qt

--- usr/share/ublue-os/firstboot/launcher/login-profile.sh
+++ usr/share/ublue-os/firstboot/launcher/login-profile.sh
@@ -4,8 +4,8 @@ if [ "$(id -u)" != "0" ] && [ ! -z "$HOME" ] && [ -d "$HOME" ]; then
     if [ ! -f "$HOME"/.config/ublue-firstboot ]; then
         # copy config files
         cp -r /usr/etc/homedir/* "$HOME"/
-        cp -r /usr/etc/homedir/.config/ "$HOME"/.config/
-        cp -r /usr/etc/homedir/.local/share/ "$HOME"/.local/share/
+        cp -r /usr/etc/homedir/.config/* "$HOME"/.config/
+        cp -r /usr/etc/homedir/.local/share/* "$HOME"/.local/share/
     fi
 
     if [[ -z $DISPLAY ]] && [[ $(tty) = /dev/tty1 ]]; then
