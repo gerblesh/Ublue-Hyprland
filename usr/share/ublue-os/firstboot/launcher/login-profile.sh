e5965104748c112600f2b23848e9de2b1857cfd9
gerblesh <101901964+gerblesh@users.noreply.github.com>
2023-05-21 00:24:49 -0700

    fix more issues with first login, tweaked yafti config

--- usr/share/ublue-os/firstboot/launcher/login-profile.sh
+++ usr/share/ublue-os/firstboot/launcher/login-profile.sh
@@ -1,10 +1,11 @@
 # Only process users with home directories, but skip the "root" user.
 if [ "$(id -u)" != "0" ] && [ ! -z "$HOME" ] && [ -d "$HOME" ]; then
+
     if [ ! -f "$HOME"/.config/ublue-firstboot ]; then
         # copy config files
-        cp -r /usr/etc/homedir/* ~HOME/
-        cp -r /usr/etc/homedir/.config/ ~/.config/
-        cp -r /usr/etc/homedir/.local/share/ ~/.local/share/
+        cp -r /usr/etc/homedir/* "$HOME"/
+        cp -r /usr/etc/homedir/.config/ "$HOME"/.config/
+        cp -r /usr/etc/homedir/.local/share/ "$HOME"/.local/share/
     fi
 
     if [[ -z $DISPLAY ]] && [[ $(tty) = /dev/tty1 ]]; then
