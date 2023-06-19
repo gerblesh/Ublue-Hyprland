9ff80becd92e231c11e3553d882d4599de78c177
gerblesh <101901964+gerblesh@users.noreply.github.com>
2023-06-19 13:15:36 -0700

    fixed fish config

--- usr/etc/homedir/.config/fish/config.fish
+++ usr/etc/homedir/.config/fish/config.fish
@@ -2,8 +2,10 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
 end
 function fish_greeting
-
+# blank fish greeting
 end
+
 alias mpv="flatpak run io.mpv.Mpv"
 alias ls="exa --icons --sort type"
-set PATH /usr/bin:/usr/local/bin/:/usr/sbin:/bin:/sbin:/home/user/.local/bin:/home/user/.cargo/bin:$PATH
+set PATH /var/home/user/.local/bin:$PATH
+
