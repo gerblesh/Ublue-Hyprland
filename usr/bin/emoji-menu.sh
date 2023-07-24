a09d98abfefab0eb3f4c229e479f167b71dfb44e
gerblesh <101901964+gerblesh@users.noreply.github.com>
2023-07-24 00:15:02 -0700

    improve emoji script

--- usr/bin/emoji-menu.sh
+++ usr/bin/emoji-menu.sh
@@ -6,11 +6,12 @@ mkdir -p "$cache_home"
 data_file="$cache_home/emoji-menu.json"
 
 if ! [ -f "$data_file" ]; then
-	curl -o "$data_file" -L "https://github.com/github/gemoji/raw/master/db/emoji.json"
+    curl -o "$data_file" -L "https://github.com/github/gemoji/raw/master/db/emoji.json"
 fi
 
 filter='.[] | (.emoji + " " + .description + " (" + (.aliases | join(", ")) + ")")'
 sel="$(jq -r "$filter" <"$data_file" | fuzzel --dmenu --prompt " Select Emoji:  " | cut -d ' ' -f 1 | tr -d '\n')"
 if [ -n "$sel" ]; then
-	wl-copy "$sel"
+    wl-copy "$sel"
+    wtype "$sel"
 fi
