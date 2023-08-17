d97bd527d3ef5354ba888bb0b4d7dd709552938b
gerblesh <101901964+gerblesh@users.noreply.github.com>
2023-08-16 18:32:32 -0700

    switch to newer waybar

--- scripts/post/setup-systemd-services.sh
+++ scripts/post/setup-systemd-services.sh
@@ -2,6 +2,8 @@
 
 set -oue pipefail
 
+rpm-ostree override replace https://bodhi.fedoraproject.org/updates/FEDORA-2023-785fb9cf29
+
 echo "Setup systemd units"
 systemctl enable getty@tty1
 systemctl disable rpm-ostree-countme.service
