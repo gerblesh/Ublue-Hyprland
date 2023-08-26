8146f05e8cd31a340af3ebb578a7c9d5f1d7cb2e
gerblesh <101901964+gerblesh@users.noreply.github.com>
2023-08-26 14:47:45 -0700

    asdjfkalsdfjaslfkj

--- usr/bin/wallpaper-menu.sh
+++ usr/bin/wallpaper-menu.sh
@@ -2,5 +2,3 @@
 NEW_WALLPAPER=$(/usr/bin/ls -1 "$HOME/Pictures/Wallpapers/Mocha" | fuzzel --dmenu --prompt " Select Wallpaper:  ")
 
 /usr/bin/wallpaper "$HOME/Pictures/Wallpapers/Mocha/$NEW_WALLPAPER"
-
-
