3a7c3a46bc978066bcde280f84fea4e0f31eafc6
gerblesh <101901964+gerblesh@users.noreply.github.com>
2023-09-16 18:57:35 -0700

    fix shell formatting

--- config/scripts/install-mullvad.sh
+++ config/scripts/install-mullvad.sh
@@ -1,7 +1,14 @@
+#!/usr/bin/env bash
 
-mkdir -p /tmp/rpms
+# Tell this script to exit if there are any errors.
+# You should have this in every custom script, to ensure that your completed
+# builds actually ran successfully without any errors!
+set -oue pipefail
+
+mkdir -p /tmp/mullvad
 
 #curl -Lo /tmp/rpms/mullvad-vpn.rpm https://mullvad.net/en/download/app/rpm/latest --max-redirs 1
+
 echo 'Downloading files needed for installation ...'
 wget -qnc --trust-server-names https://mullvad.net/download/app/rpm/latest -P /tmp/mullvad
 wget -qnc --trust-server-names https://mullvad.net/download/app/rpm/latest/signature -P /tmp/mullvad
